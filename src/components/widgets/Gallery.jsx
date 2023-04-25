924239f96e3fc355e084a19d18467c76827271e1
grantbirki <grantbirki@github.com>
2023-04-25 14:24:42 +0100

    better image gallery

--- src/components/widgets/Gallery.jsx
+++ src/components/widgets/Gallery.jsx
@@ -1,48 +1,19 @@
-// gallery reference: https://tailwind-elements.com/docs/standard/components/gallery/
+// gallery reference: https://react-photo-album.com/
 
-import ModalImage from 'react-modal-image';
-
-const modalCss =
-  'block h-full w-full rounded-lg object-cover object-center transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/20';
+import PhotoAlbum from 'react-photo-album';
 
 export default function Gallery(props) {
-  // check if the screen width is less than 700px
-  // this isn't responsive and requires a page refresh but that is fine for now
-  const isMobile = window.innerWidth < 700;
-  var imageBoxCss = 'flex w-1/3 flex-wrap';
-  if (isMobile) {
-    imageBoxCss = 'flex flex-wrap my-4';
-  }
-
-  // Use the basic gallery style if it is requested or if the screen width is less than 700px
-  if (props.style === 'basic' || isMobile) {
-    return (
-      <>
-        <div className="container mx-auto mb-20 px-5 py-2 lg:px-32 lg:pt-12">
-          <div className="-m-1 flex flex-wrap md:-m-2">
-            {props.images.map((image, index) => {
-              return (
-                <div key={index} className={imageBoxCss}>
-                  <div className="w-full p-1 md:p-2">
-                    <ModalImage
-                      small={image.small}
-                      medium={image.medium}
-                      large={image.large}
-                      alt={image.alt}
-                      className={modalCss}
-                    />
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </>
-    );
-  }
-
-  // If the gallery style is mixed and the display is not a mobile device
-  if (props.style === 'mixed' && !isMobile) {
-    // TODO
-  }
+  return (
+    <div className="container mx-auto mb-20 px-5 py-2 lg:px-32 lg:pt-12">
+      <PhotoAlbum
+        layout={props.layout}
+        photos={props.photos}
+        columns={(containerWidth) => {
+          if (containerWidth < 400) return 1;
+          if (containerWidth < 800) return 2;
+          return 3;
+        }}
+      />
+    </div>
+  );
 }
--- src/config/pages/gallery/gallery.js
+++ src/config/pages/gallery/gallery.js
@@ -5,45 +5,51 @@ const gallery = {
   enabled: true,
 
   // The gallery style (basic or mixed)
-  style: 'basic',
+  layout: 'masonry',
 
   // Collection of images to display in the gallery
-  images: [
+  photos: [
     {
+      key: 1,
       alt: 'Hello World',
-      small: '/public/assets/colors-public.jpg',
-      medium: '/public/assets/colors-public.jpg',
-      large: '/public/assets/colors-public.jpg',
+      src: '/assets/colors-public.jpg',
+      width: 1920,
+      height: 1280,
     },
     {
+      key: 2,
       alt: 'Hello World',
-      small: '/public/assets/ring-public.jpg',
-      medium: '/public/assets/ring-public.jpg',
-      large: '/public/assets/ring-public.jpg',
+      src: '/assets/ring-public.jpg',
+      width: 1600,
+      height: 2400,
     },
     {
+      key: 3,
       alt: 'Hello World',
-      small: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      medium: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      large: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      src: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      width: 100,
+      height: 100,
     },
     {
+      key: 4,
       alt: 'Hello World',
-      small: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      medium: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      large: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      src: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      width: 100,
+      height: 100,
     },
     {
+      key: 5,
       alt: 'Hello World',
-      small: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      medium: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      large: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      src: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      width: 100,
+      height: 100,
     },
     {
+      key: 6,
       alt: 'Hello World',
-      small: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      medium: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
-      large: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      src: 'https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp',
+      width: 100,
+      height: 100,
     },
   ],
 };
