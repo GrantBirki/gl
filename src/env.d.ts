99c97db816c1ef5b390ab865400a356f2308f1a1
grantbirki <grantbirki@github.com>
2023-04-25 12:44:03 +0100

    put all images inside of a clickable modal

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,18 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+
+declare module 'react-modal-image' {
+  export default function ModalImage(props: {
+    small?: string;
+    medium?: string;
+    large?: string;
+    smallSrcSet?: string;
+    hideDownload?: boolean;
+    hideZoom?: boolean;
+    showRotate?: boolean;
+    imageBackgroundColor?: string;
+    alt?: string;
+    className?: string;
+  }): JSX.Element;
+}
