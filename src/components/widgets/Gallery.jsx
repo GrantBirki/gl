8bb20ca52ad9f761462132a44674c288f56b8429
grantbirki <grantbirki@github.com>
2023-04-25 13:18:28 +0100

    use className

--- src/components/widgets/Gallery.jsx
+++ src/components/widgets/Gallery.jsx
@@ -8,12 +8,12 @@ const modalCss =
 export default function Gallery(props) {
   return (
     <>
-      <div class="container mx-auto mb-20 px-5 py-2 lg:px-32 lg:pt-12">
-        <div class="-m-1 flex flex-wrap md:-m-2">
+      <div className="container mx-auto mb-20 px-5 py-2 lg:px-32 lg:pt-12">
+        <div className="-m-1 flex flex-wrap md:-m-2">
           {props.images.map((image, index) => {
             return (
               <div key={index} className="flex w-1/3 flex-wrap">
-                <div class="w-full p-1 md:p-2">
+                <div className="w-full p-1 md:p-2">
                   <ModalImage
                     small={image.small}
                     medium={image.medium}
