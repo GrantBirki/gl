07ad195ed5357231c8bf3d54ff9cadb245b024e5
grantbirki <grantbirki@github.com>
2023-05-17 20:45:48 +0100

    make the globe responsive

--- src/components/widgets/GlobeView.jsx
+++ src/components/widgets/GlobeView.jsx
@@ -1,4 +1,5 @@
 import Globe from 'react-globe.gl';
+import { SizeMe } from 'react-sizeme';
 import map from '../../config/components/map.json';
 
 export default function GlobeView() {
@@ -13,23 +14,28 @@ export default function GlobeView() {
   }));
 
   return (
-    <Globe
-      globeImageUrl={'/assets/globe.jpg'}
-      pointsData={map}
-      labelsData={map}
-      labelLat="lat"
-      labelLng="lon"
-      labelDotOrientation={() => 'bottom'}
-      labelText="name"
-      labelColor={() => 'rgba(255, 165, 0, 0.75)'}
-      labelResolution={2}
-      labelSize={1}
-      labelDotRadius={1}
-      arcsData={arcsData} // flying arcs
-      arcColor={'color'} // flying arcs
-      arcDashLength={() => Math.random()} // flying arcs
-      arcDashGap={1} // flying arcs
-      arcDashAnimateTime={() => Math.random() * 4000 + 2000} // flying arcs
-    />
+    <SizeMe>
+      {({ size }) => (
+        <Globe
+          width={size.width}
+          globeImageUrl={'/assets/globe.jpg'}
+          pointsData={map}
+          labelsData={map}
+          labelLat="lat"
+          labelLng="lon"
+          labelDotOrientation={() => 'bottom'}
+          labelText="name"
+          labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+          labelResolution={2}
+          labelSize={1}
+          labelDotRadius={1}
+          arcsData={arcsData} // flying arcs
+          arcColor={'color'} // flying arcs
+          arcDashLength={() => Math.random()} // flying arcs
+          arcDashGap={1} // flying arcs
+          arcDashAnimateTime={() => Math.random() * 4000 + 2000} // flying arcs
+        />
+      )}
+    </SizeMe>
   );
 }
