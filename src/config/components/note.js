7144195b371c143cd6a42fd4824e95a64270c923
GrantBirki <grantbirki@github.com>
2024-03-10 22:20:32 -0600

    dates

--- src/config/components/note.js
+++ src/config/components/note.js
@@ -1,11 +1,13 @@
 // Configuration for the note component
 
+import { SITE } from '~/config/site/config.js';
+
 const note = {
   // If the note component should be enabled or not
   enabled: true,
 
   // Note text with HTML support
-  text: `<span class="font-bold">Save the Date:</span> G + L are getting married on YYYY-MM-DD`,
+  text: `<span class="fancy">G + L are getting married on ${SITE.weddingDate}</span>`,
 
   // Optional note icon (prefix to the text)
   icon: {
